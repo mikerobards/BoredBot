78e84d87ed3d90866d961b6ec02a2f1f31ecc096
Michael Robards <mikerobards@icloud.com>
2021-06-17 16:37:00 -0400

    first borded api

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 
+// fetch('https://dog.ceo/api/breeds/image/random')
+//     .then(response => response.json())
+//     .then(data => document.body.innerHTML = `<img src=${data.message}>`);
 
-fetch('https://dog.ceo/api/breeds/image/random')
+const bored = document.getElementById('bored')
+
+fetch('https://apis.scrimba.com/bored/api/activity')
     .then(response => response.json())
-    .then(data => document.body.innerHTML = `<img src=${data.message}>`);
+    .then(data => {
+        bored.textContent = `${data.activity}`
+    })
