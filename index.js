b0303fb0c24280bc5d678315c86080e0266a78ca
Michael Robards <mikerobards@icloud.com>
2021-06-17 16:24:05 -0400

    get dog img

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 
-function getDog() {
-    fetch('https://dog.ceo/api/breeds/image/random')
-        .then(response => response.json())
-        .then(data => location.href = data.message);
-}
-const myBtn = document.getElementById('btn')
 
-myBtn.addEventListener('click', getDog)
+fetch('https://dog.ceo/api/breeds/image/random')
+    .then(response => response.json())
+    .then(data => document.body.innerHTML = `<img src=${data.message}>`);
