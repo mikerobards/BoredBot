837883ef67e1e122aae1adbf142a8266d00a938b
Michael Robards <mikerobards@icloud.com>
2021-06-17 16:44:01 -0400

    add html text and btn functionality

--- index.js
+++ index.js
@@ -3,10 +3,15 @@
 //     .then(response => response.json())
 //     .then(data => document.body.innerHTML = `<img src=${data.message}>`);
 
-const bored = document.getElementById('bored')
+const ideaText = document.getElementById('idea-text')
+const ideaBtn = document.getElementById('idea-btn')
 
-fetch('https://apis.scrimba.com/bored/api/activity')
-    .then(response => response.json())
-    .then(data => {
-        bored.textContent = `${data.activity}`
-    })
+ideaBtn.addEventListener('click', () => {
+    fetch('https://apis.scrimba.com/bored/api/activity')
+        .then(response => response.json())
+        .then(data => {
+            ideaText.textContent = `${data.activity}`
+        })
+})
+
+
